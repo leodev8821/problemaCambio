2c8c91d57b358e4169bd934ac64d9f15ad9e9633
leodev8821 <158272474+leodev8821@users.noreply.github.com>
2024-10-23 17:29:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let caja = [
     /*    0     1    2   3   4   5   6  7  8    9     10    11    12   13     14*/
         [500, 200, 100, 50, 20, 10,  5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 ],// Denominación
-        [ 0,   0,   0 , 1 ,  4,  8 , 2, 5 ,4 ,  2 ,  0 ,   1 ,    2 ,  3 ,   1],  //cantidades de billetes
+        [ 0,   0,   0 , 1 ,  4,  8 , 2, 5 ,4 ,  0 ,  0 ,   1 ,    2 ,  3 ,   1],  //cantidades de billetes
 ];
 
 let pago = [
@@ -14,7 +14,7 @@ let devolver = [
     [  0,  0,   0,   0,  0,  0, 0, 0, 0,  0,   0,   0,    0,   0,    0]
 ];
 
-let importe = 2.5; //precio del articulo
+let importe = 2.85; //precio del articulo
 
 function calculaTotal(efectivo){
     let total = 0.0;
@@ -42,6 +42,7 @@ function hayCambio(efectivo, cambio, posEfectivo){
     let nBilletes = 0;
 
     while (cambio > 0 && posEfectivo < efectivo[1].length) {
+        cambio = parseFloat(cambio.toFixed(2));
         console.log("cambio: " + cambio);
         console.log("pos: " + posEfectivo);
         nBilletes = Math.trunc((cambio / efectivo[0][posEfectivo]));
@@ -50,9 +51,8 @@ function hayCambio(efectivo, cambio, posEfectivo){
             cambio -= efectivo[0][posEfectivo] * nBilletes;
             devolver[1][posEfectivo] = nBilletes;
             caja[1][posEfectivo] -= nBilletes
-            billetesDevueltos(devolver);
+            billetesDevueltos(devolver);           
             console.log('devolver: '+devolver[1]);
-            console.log('Caja despues de cambio: '+caja)
             posEfectivo = buscar(efectivo, cambio);
         }else{
             posEfectivo++;
@@ -88,11 +88,26 @@ if(pago[0][pago[0].length - 1] - importe == 0){ //Pago justo
     }else{
         if(pago[0][pago[0].length - 1] < importe){  //Pago menor al precio del artículo
             alert('Falta dinero para poder comprar');
-        }else{
-            let posCaja = buscar(caja, 47.5);
-            if(hayCambio(caja, 47.5, posCaja)){
+        }else{                                         // Hay cambio y se devuelve dinero
+            let posCaja = buscar(caja, 47.15);
+            let valorPago = 0;
+
+            if(hayCambio(caja, 47.15, posCaja)){
                 alert('Hay cambio');
                 alert(billetesDevueltos(devolver));
+
+                // Para sacar del array la cantidad pagada
+                for(let i = 0; i < pago[1].length; i++){
+                    if(pago[1][i] > 0){
+                        valorPago += pago[0][i];
+                        caja[1][i] += pago[1][i];
+                        console.log('Valor de pago: '+valorPago)
+                        console.log('Caja con nuevos billetes: '+caja[1]);
+                    }
+                } 
+                caja[0][15] = caja[0][15] + importe;
+                console.log('Caja despues de cambio: '+caja[1])
+                alert('Nuevo valor de caja es: '+caja[0][15]);
             }else{
                 alert('No hay cambio')
             }
